4f0deb3bf8e44fc200e658a2fda110eea69e428b
aseel.a <aseel.a@wtm.llc>
2025-06-24 23:41:45 +0300

    add config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthController } from './auth.controller';
 import { PrismaService,PrismaModule } from 'nestjs-prisma';
 import { TokenService } from 'src/token/token.service';
 import { JwtService } from '@nestjs/jwt';
+
 @Module({
   imports: [PrismaModule],
   providers: [AuthService, PrismaService, TokenService, JwtService],
---
+++ src/config/configuration.interface.ts
@@ -0,0 +1,11 @@
+export interface EnvVariables {
+    ENV : string
+    DATABASE_URL : string
+    PORT : number
+    JWT_SECRET : string
+
+    
+
+
+
+}
